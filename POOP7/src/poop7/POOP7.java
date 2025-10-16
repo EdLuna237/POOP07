d7293e78c3bba613e7f5346e2a65a2ca72a8d8be
EdLuna237 <edgar.lusan237@gmail.com>
2025-10-16 00:04:35 -0600

    Add files via upload

---
+++ POOP7/src/poop7/POOP7.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poop7;
+import animales.Ballena;
+
+/**
+ *
+ * @author Reid_
+ */
+public class POOP7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Empleado emp1 = new Empleado("Juan",18,30000);
+        System.out.println(emp1.getNombre());
+        System.out.println(emp1.toString());
+        
+        Gerente emp2 = new Gerente(200000,"Luis",20,40000);
+        System.out.println(emp2.toString());
+        
+        Ballena ballena1 = new Ballena(20, 2, "moby", "atlantico", "gris");
+        ballena1.pelearconPinocho();
+        System.out.println(ballena1.toString());
+        ballena1.sonido();
+    }
+    
+}
